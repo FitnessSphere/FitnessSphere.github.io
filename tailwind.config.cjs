86831fcf192b76dc1b85bf26671b535a18af22f6
FitnessSphere <recallscans@gmail.com>
2025-08-11 22:29:34 +0530

    Delete tailwind.config.cjs

--- tailwind.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: { colors: { primary:'#6C5CE7', accent:'#00B894' }, borderRadius: { lg: '16px' } } },
-  plugins: [],
-}
