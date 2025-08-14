2735cd1376a4faba413e27d2d24b8c4c4ed92583
FitnessSphere <recallscans@gmail.com>
2025-08-14 12:57:33 +0530

    Delete Blog.jsx

--- Blog.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Blog(){return (
-<section className="py-12">
-  <div className="max-w-4xl mx-auto px-4">
-    <h2 className="text-2xl font-bold mb-4">Blog & Guides</h2>
-    <div className="card p-4 bg-white">Sample article: How to start with bodyweight training.</div>
-  </div>
-</section>
-)}
